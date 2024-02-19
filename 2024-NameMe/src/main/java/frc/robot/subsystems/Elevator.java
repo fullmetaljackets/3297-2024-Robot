bfe1c260d5d600d8096d88e48ace59d9982e9e93
fullmetaljackets <100058498+fullmetaljackets@users.noreply.github.com>
2024-02-19 17:27:01 -0600

    Display Elevator PID values

--- 2024-NameMe/src/main/java/frc/robot/subsystems/Elevator.java
+++ 2024-NameMe/src/main/java/frc/robot/subsystems/Elevator.java
@@ -16,7 +16,10 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -28,6 +31,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Elevator extends SubsystemBase{ 
     private CANSparkMax elevatorMotor;
 
+    // FEP (Find Elevator Position)
+    private SparkPIDController m_pidController;
+    private RelativeEncoder m_encoder;
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+    // FEP end
+
     
     /**
     *
@@ -38,7 +47,46 @@ public class Elevator extends SubsystemBase{
         elevatorMotor.setInverted(false);
         elevatorMotor.setIdleMode(IdleMode.kBrake);
         elevatorMotor.burnFlash();
-    }
+   
+        // FEP start
+        /**
+         * In order to use PID functionality for a controller, a SparkPIDController object
+         * is constructed by calling the getPIDController() method on an existing
+         * CANSparkMax object
+         */
+        m_pidController = elevatorMotor.getPIDController();
+
+        // Encoder object created to display position values
+        m_encoder = elevatorMotor.getEncoder();
+
+        // PID coefficients
+        kP = 0.1; 
+        kI = 1e-4;
+        kD = 1; 
+        kIz = 0; 
+        kFF = 0; 
+        kMaxOutput = 1; 
+        kMinOutput = -1;
+
+        // set PID coefficients
+        m_pidController.setP(kP);
+        m_pidController.setI(kI);
+        m_pidController.setD(kD);
+        m_pidController.setIZone(kIz);
+        m_pidController.setFF(kFF);
+        m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("P Gain", kP);
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("I Zone", kIz);
+        SmartDashboard.putNumber("Feed Forward", kFF);
+        SmartDashboard.putNumber("Max Output", kMaxOutput);
+        SmartDashboard.putNumber("Min Output", kMinOutput);
+        SmartDashboard.putNumber("Set Rotations", 0);
+        // FEP end
+   }
 
     @Override
     public void periodic() {
@@ -56,6 +104,49 @@ public class Elevator extends SubsystemBase{
     // here. Call these from Commands.
     public void my_motorrun(double setpoint){
         elevatorMotor.set(setpoint);
+
+        // FEP start
+        // read PID coefficients from SmartDashboard
+        double p = SmartDashboard.getNumber("P Gain", 0);
+        double i = SmartDashboard.getNumber("I Gain", 0);
+        double d = SmartDashboard.getNumber("D Gain", 0);
+        double iz = SmartDashboard.getNumber("I Zone", 0);
+        double ff = SmartDashboard.getNumber("Feed Forward", 0);
+        double max = SmartDashboard.getNumber("Max Output", 0);
+        double min = SmartDashboard.getNumber("Min Output", 0);
+        double rotations = SmartDashboard.getNumber("Set Rotations", 0);
+
+        // if PID coefficients on SmartDashboard have changed, write new values to controller
+        if((p != kP)) { m_pidController.setP(p); kP = p; }
+        if((i != kI)) { m_pidController.setI(i); kI = i; }
+        if((d != kD)) { m_pidController.setD(d); kD = d; }
+        if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
+        if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+        if((max != kMaxOutput) || (min != kMinOutput)) { 
+            m_pidController.setOutputRange(min, max); 
+            kMinOutput = min; kMaxOutput = max; 
+        }
+
+        /**
+         * PIDController objects are commanded to a set point using the 
+         * SetReference() method.
+         * 
+         * The first parameter is the value of the set point, whose units vary
+         * depending on the control type set in the second parameter.
+         * 
+         * The second parameter is the control type can be set to one of four 
+         * parameters:
+         *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
+         *  com.revrobotics.CANSparkMax.ControlType.kPosition
+         *  com.revrobotics.CANSparkMax.ControlType.kVelocity
+         *  com.revrobotics.CANSparkMax.ControlType.kVoltage
+         */
+        m_pidController.setReference(rotations, CANSparkMax.ControlType.kPosition);
+        
+        SmartDashboard.putNumber("SetPoint", rotations);
+        SmartDashboard.putNumber("ProcessVariable", m_encoder.getPosition());
+        // FEP end
+
         //DriverStation.reportError("******** elevatorMotor **************", false);
     }
 }
